662a27ba0b739cb6fb618c0bcf46c05c4c6ebe84
VANOLA <62918869+VANOLA@users.noreply.github.com>
2021-11-16 09:17:38 -0600

    Removed outliers Population Data

--- homework/main.py
+++ homework/main.py
@@ -8,6 +8,7 @@ import csv
 import matplotlib.pyplot as plt
 import re
 import pandas as pd
+import numpy as np
 
 
 def show_menu():
@@ -18,13 +19,6 @@ def show_menu():
     while not flag:
         print('a - Population Data\nb - Housing Data\nc - Exit the Program')
         user_choice = input(': ')
-        # if choice == 'a':
-        #     return 'a'
-        # if choice == 'b':
-        #     return 'b'
-        # if choice == 'c':
-
-        #     return 'c'
         if re.findall(r'[a-c]', user_choice):
             flag = True
         else:
@@ -44,16 +38,57 @@ def get_choice_pop():
     return user_choice
 
 
+def get_choice_housing():
+    """Gets and verifies the users choice for data analysis"""
+    flag = False
+    while not flag:
+        user_choice = input(': ').lower()
+        if re.findall(r'[a-f]', user_choice):
+            flag = True
+        else:
+            print('Please enter a valid letter')
+    return user_choice
+
+
+def find_quartiles(df):
+    """Finds the statistical dispersion"""
+    q1 = df.quantile(0.25)
+    q3 = df.quantile(0.75)
+    iqr = q3 - q1
+    return iqr
+
+
+def trim_data_pop(df, col):
+    """Trims outliers from the dataframe"""
+    ind = df[df[col] >= 150_000]
+    print(ind)
+    return ind
+
+
+def trim_data_housing(df, col):
+    """Trims outliers from the dataframe"""
+    ind = df[df[col] <= 0]
+    print(ind)
+    return ind
+
+
+def plot_histogram(data_frame, column):
+    """Plots a histogram from the given data frame and column"""
+    data_frame.hist(column=column, grid=True)
+    plt.show()
+    plt.close()
+
+
 print('*************Welcome to the Python Data Analysis App*************')
 
-while True:
+while True:  # runs as long as user would like to use program
     # Calls show_menu and opens the correct csv file
     which_file = show_menu()
-    if which_file == 'c':  # exits program
-        print('Thank you for using the Python Data Analysis App')
-        break
+
     if which_file == 'a':  # population data choice
         print('You have chosen Population Data')
+        # opens the csv and loads data into a data frame from analyzing
+        pop_frame = pd.read_csv('PopChange.csv')
 
         # allows user to choose a column to analyze
         while True:
@@ -61,21 +96,85 @@ while True:
             print('a Pop Apr 1\nb Pop Jul 1\nc Change Pop\nd Exit Column')
             pop_op = get_choice_pop()
 
-            # opens the csv and loads data into a data frame from analyzing
-            pop_frame = pd.read_csv('PopChange.csv')
-            pd.set_option('display.max_columns', None)
-            # print(pop_frame)
+            # Find the indices of outliers in the data frame
+            print('Outliers: ')
+            trim_data_pop(pop_frame, 'Pop Apr 1')
 
-            # execute the correct analysis based on user choice
+            # executes the correct analysis based on user choice
             if pop_op == 'a':
                 print('You selected Pop April 1')
                 print('The statistics for this column are:')
                 print(pop_frame['Pop Apr 1'].describe())
-                # hist = pop_frame['Pop Apr 1'].hist()
-                hist = pop_frame.hist(column='Pop Apr 1', grid=True)
-                plt.show()
-                plt.close()
+                pop_frame.drop(labels=[97, 223, 377, 501, 550], axis=0,
+                               inplace=True)  # drops the outliers
+                plot_histogram(pop_frame, 'Pop Apr 1')
+            if pop_op == 'b':
+                print('You selected Pop Jul 1')
+                print('the statistics for this column are:')
+                print(pop_frame['Pop Jul 1'].describe())
+                pop_frame.drop(labels=[97, 223, 377, 501, 550], axis=0,
+                               inplace=True)  # drops the outliers
+                plot_histogram(pop_frame, 'Pop Jul 1')
+            if pop_op == 'c':
+                print('You selected Change in Pop')
+                print('The statistics for this column are:')
+                print(pop_frame['Change Pop'].describe())
+                pop_frame.drop(labels=[97, 223, 377, 501, 550], axis=0,
+                               inplace=True)  # drops the outliers
+                plot_histogram(pop_frame, 'Change Pop')
             if pop_op == 'd':
                 print('Thank you for choosing Population Data')
                 break
 
+    if which_file == 'b':  # Housing data choice
+        print('You have chose Housing Data')
+        # opens the csv and loads data into a data frame for analyzing
+        housing_frame = pd.read_csv('Housing.csv')
+
+        # allows a user to choose a column to analyze
+        while True:
+            print('Select the column you want to analyze: ')
+            print('a Age\nb Bedrooms\nc Year Built\nd Rooms\ne Utility\n'
+                  'f Exit Column')
+            housing_op = get_choice_housing()
+
+            # Find the indices of outliers in the data frame
+            print('Outliers: ')
+            trim_data_housing(housing_frame, 'AGE')
+
+            q1 = housing_frame.quantile(0.25)
+            q3 = housing_frame.quantile(0.75)
+            iqr = q3 - q1
+
+            if housing_op == 'a':
+                print('You selected Age')
+                print('the statistics for this column are:')
+                print(housing_frame['AGE'].describe())
+                plot_histogram(housing_frame, 'AGE')
+
+            if housing_op == 'b':
+                print('You selected Bedrooms')
+                print('the statistics for this column are:')
+                print(housing_frame['BEDRMS'].describe())
+                plot_histogram(housing_frame, 'BEDRMS')
+            if housing_op == 'c':
+                print('You selected Year Built')
+                print('the statistics for this column are:')
+                print(housing_frame['BUILT'].describe())
+                plot_histogram(housing_frame, 'BUILT')
+            if housing_op == 'd':
+                print('You selected Rooms')
+                print('the statistics for this column are:')
+                print(housing_frame['ROOMS'].describe())
+                plot_histogram(housing_frame, 'ROOMS')
+            if housing_op == 'e':
+                print('Utility')
+                print('the statistics for this column are:')
+                print(housing_frame['UTILITY'].describe())
+                plot_histogram(housing_frame, 'UTILITY')
+            if housing_op == 'f':
+                print('Thank you for choosing Housing Data')
+                break
+    if which_file == 'c':  # exits program
+        print('Thank you for using the Python Data Analysis App')
+        break
